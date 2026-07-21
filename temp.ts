eda576f1210ac5fe925192c8aeddc4f4e2a03afa
Sayandeep-the-coder <jitsaha951@gmail.com>
2026-07-21 13:39:30 +0530

    Remove temp file

--- temp.ts
+++
@@ -1,9 +0,0 @@
-import postgres from 'postgres';
-import { env } from './lib/config/env';
-
-const sql = postgres(env.DATABASE_URL);
-
-sql`select * from users where github_id = '182542659' limit 1`
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => process.exit(0));
